80d64d2c2d52275b343552c7c89c0c98cd0386b7
tami <xorud7979@gmail.com>
2022-09-13 17:53:47 +0900

    feat: Next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+  async redirects(){
+    return[
+      {
+        source:"/contact",
+        destination:"/form",
+        permanent: false,
+      }
+    ]
+  }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import NavBar from "../components/NavBar";
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          a {
+            font-size: 30px;
+          }
+        `}
+      </style>
+    </>
+  );
 }
-
-export default MyApp
